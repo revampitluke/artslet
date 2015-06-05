435d624b33c0dc4a35781f5b164a6c34d279c922
labeetotrent <trentbrookes@gmail.com>
2015-06-05 12:57:59 +1000

    added platinum booking

---
+++ platinum/configuration.php
@@ -0,0 +1,21 @@
+<?php
+class SystemConfiguration {
+    // General Settings
+    public static $base_url    = 'http://my.thetelstrastore.com.au/platinum';
+    
+    // Database Settings
+    public static $db_host     = 'labeeto3.cloudapp.net';
+    public static $db_name     = 'artslet';
+    public static $db_username = 'artslet';
+    public static $db_password = 'dale4152';
+    
+    // Google Calendar API Settings
+    public static $google_sync_feature  = FALSE; // Enter TRUE or FALSE;
+    public static $google_product_name  = '';
+    public static $google_client_id     = '';
+    public static $google_client_secret = '';
+    public static $google_api_key       = '';
+}
+
+/* End of file configuration.php */
+/* Location: ./configuration.php */
